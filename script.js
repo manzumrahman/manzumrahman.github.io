1a041fee29c53a77814077510c0f79903dce9ec3
manzumrahman <manzumrahman@gmail.com>
2024-06-19 02:28:05 +0600

    optimized for mobile devices, fixed the navbar and added animations + Hover effect

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const toggle_button = document.getElementsByClassName('toggle-button')[0]
+const toggle_button_main = document.getElementsByClassName('toggle-button-main')[0]
 toggle_button.addEventListener('click', () =>{
     const navbar_nav = document.getElementsByClassName('navbar-nav')[0]
     const navbar = document.getElementsByClassName('navbar')[0]
     navbar_nav.classList.toggle('active')
     navbar.classList.toggle('active')
+    toggle_button_main.classList.toggle('fa-x');
+    toggle_button_main.classList.toggle('fa-bars');
+    
 })
+AOS.init();
