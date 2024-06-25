8af655d8e49e62fefdd7b4eadd41da412adec829
manzumrahman <manzumrahman@gmail.com>
2024-06-25 14:49:46 +0600

    Added a preloader

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+var loader = document.getElementById('preloader')
+window.addEventListener('load', function(){
+        loader.style.display="none"
+    })
+
+
 const toggle_button = document.getElementsByClassName('toggle-button')[0]
 const toggle_button_main = document.getElementsByClassName('toggle-button-main')[0]
 toggle_button.addEventListener('click', () =>{
